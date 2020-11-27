78cc961b214ad1455bc51fbedd4598fdb2376c85
Gene Boggs <gene@ology.net>
2020-11-27 13:56:05 -0800

    Add handy comments

--- ex-08-04.pl
+++ ex-08-04.pl
@@ -8,23 +8,29 @@ use Music::Note;
 
 my $mcp = Music::Chord::Positions->new;
 
-my $inversion = shift || 1;
+my $inversion = shift || 1; # Or 2
 
+# Define the initial chord
 my @chord = qw(C4 E4 G4);
 print 'Chord: ', ddc(\@chord);
 
+# Get the list of octaves for each note
 my @octaves = map { s/[A-G]//r } @chord;
 print 'Octaves: ', ddc(\@octaves);
 
+# Get the list of note names without octaves
 my @isobase = map { s/\d//r } @chord;
 print 'ISObase: ', ddc(\@isobase);
 
+# Get the pitch class
 my @pitches = map { Music::Note->new($_ . -1, 'ISO')->format('midinum') } @isobase;
 print 'Pitches: ', ddc(\@pitches);
 
+# Invert the pitches
 my $inverted = $mcp->chord_inv(\@pitches, inv_num => $inversion);
 print 'Inverted: ', ddc($inverted);
 
+# Convert the inverted pitches back to named notes
 my @notes = map { Music::Note->new($_, 'midinum')->format('ISO') } @$inverted;
 print 'New: ', ddc(\@notes);
 
