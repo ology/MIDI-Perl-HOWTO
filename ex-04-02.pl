ff7ce03869f1d8ba9720ee9d1486bfdbb24b205b
Gene Boggs <gene@ology.net>
2020-11-26 23:16:53 -0800

    Initial commit

---
+++ ex-04-02.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use MIDI::Util;
+use Music::Scales qw(get_scale_MIDI);
+
+my $score = MIDI::Util::setup_score(bpm => 120, volume => 120);
+
+$score->synch(\&bass, \&treble);
+
+$score->write_score("$0.mid");
+
+sub bass {
+  MIDI::Util::set_chan_patch($score, 0, 35);
+
+  my @pitches = get_scale_MIDI('C', 2, 'pentatonic');
+
+  for my $n (1 .. 4) {
+    my $pitch = $pitches[int rand @pitches];
+    $score->n('hn', $pitch);
+  }
+}
+
+sub treble {
+  MIDI::Util::set_chan_patch($score, 1, 0);
+
+  my @pitches = get_scale_MIDI('C', 4, 'major');
+
+  for my $note (1 .. 4) {
+    my $pitch = $pitches[int rand @pitches];
+    $score->n('qn', $pitch);
+    $score->r('qn');
+  }
+}
