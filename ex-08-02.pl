73f661a41f5d2a859f8aa0ab432381a20a6a5c02
Gene Boggs <gene@ology.net>
2020-11-27 09:44:22 -0800

    Initial commit

---
+++ ex-08-02.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Data::Dumper::Compact 'ddc';
+use lib map { "$ENV{HOME}/sandbox/$_/lib" } qw(MIDI-Util);
+use MIDI::Util;
+use Music::Chord::Note;
+
+my $score = MIDI::Util::setup_score();
+
+my $mcn = Music::Chord::Note->new;
+
+for my $c (qw(Cm7 F7 BbM7 EbM7 Adim7 D7 Gm)) {
+  my @chord = $mcn->chord($c);
+
+  @chord = MIDI::Util::midi_format(@chord);
+  print ddc(\@chord);
+
+  $score->n('wn', @chord);
+}
+
+$score->write_score("$0.mid");
