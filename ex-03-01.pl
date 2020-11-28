c20afec426b786b15bee91ce12b35752c8ef0997
Gene Boggs <gene@ology.net>
2020-11-28 07:51:52 -0800

    Fix bass notes

--- ex-03-01.pl
+++ ex-03-01.pl
@@ -12,15 +12,15 @@ $score->write_score("$0.mid");
 
 sub bass {
   MIDI::Util::set_chan_patch($score, 0, 35);
-  for my $n (1 .. 4) {
-    $score->n('hn', 'C3', 'v127');
+  for my $note (qw(C3 F3 G3 C4)) {
+    $score->n('hn', $note, 'v127');
   }
 }
 
 sub treble {
   MIDI::Util::set_chan_patch($score, 1, 0);
   for my $note (qw(C4 D4 E4 F4)) {
-    $score->n('qn', $note, 'v127');
+    $score->n('qn', $note, 'v120');
     $score->r('qn');
   }
 }
