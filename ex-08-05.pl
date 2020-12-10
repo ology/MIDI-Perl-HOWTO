4f80e3592868a9ae6cee82b9697fdb415d46f586
Gene Boggs <gene@ology.net>
2020-12-10 01:48:26 -0800

    Add handy comments

--- ex-08-05.pl
+++ ex-08-05.pl
@@ -12,6 +12,7 @@ my $max  = shift || 8;
 
 my $g = Graph::Directed->new;
 
+# Set our chord progression map
 my $progression = {
     1 => [qw(1 2 3 4 5 6)],
     2 => [qw(3 5)],
@@ -20,14 +21,15 @@ my $progression = {
     5 => [qw(1)],
     6 => [qw(2 4)],
 };
-my @valence = ('', 'm', 'm', '', '', 'm');
 
+# Build the graph
 for my $posn (keys %$progression) {
     for my $p ($progression->{$posn}->@*) {
         $g->add_edge($posn, $p);
     }
 }
 
+# Create a random progression
 my @progression;
 my $v = 1;
 push @progression, $v;
@@ -39,13 +41,17 @@ print "@progression\n";
 
 my @scale = get_scale_notes($note);
 
+# Set the major/minor parts of each scale position
+my @valence = ('', 'm', 'm', '', '', 'm');
+
+# Create a phrase that can be read by MIDI-Perl
 my @phrase = map { $scale[$_ - 1] . $valence[$_ - 1] } @progression;
 print "@phrase\n";
 
 my $cn = Music::Chord::Note->new;
 
+# Add octaves to the chord notes
 my @notes;
-
 for my $chord (@phrase) {
     my @chord = $cn->chord_with_octave($chord, 4);
     push @notes, \@chord;
@@ -55,6 +61,7 @@ for my $chord (@phrase) {
 
 my $score = MIDI::Util::setup_score();
 
+# Add the notes to the score
 $score->n('wn', @$_) for @notes;
 
 #$score->n('wn', $cn->chord_with_octave('C', 4));
